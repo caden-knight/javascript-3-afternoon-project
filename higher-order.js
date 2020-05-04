3d7735a2a51d59bfff035f5d64f922f3ef4cc2dc
caden-knight <cadenknight@gmail.com>
2020-05-04 04:51:04 -0600

    Almost done

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,13 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+// = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(function(value){
+  return value % 2 === 0
+})
+console.log(evenNumbers)
+//let evenNumbers = mixedNumbers.filter()
+
 
 
 
@@ -44,8 +50,8 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
+// = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(value => value * 1.07)
 
 
 ////////// PROBLEM 3 //////////
@@ -63,7 +69,10 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+//  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce((acc, curr) => {
+  return acc + curr
+})
 
 
 
@@ -89,8 +98,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
-
+// = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(value => value.CP >= 200)
+console.log(myStrongest)
 
 
 ////////// PROBLEM 5 //////////
@@ -106,7 +116,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+ // Code here
+ let orderTotals = orders.map(value => value.price * (1 + value.tax))
+ console.log(orderTotals)
 
 
 
@@ -126,6 +138,11 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
-
-
+ //Code Here
+ let bobsTotal = purchases.map((value) => {
+   if(value.owner === "Bob"){
+    console.log(value.price, value.owner)
+    return value.price
+   }
+ })
+console.log(bobsTotal)
